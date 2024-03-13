ebfc0e57c9c4597820a3f43ae1cd12980dedf7f9
leon <leon.morales@utbm.fr>
2024-03-13 12:59:39 +0100

    first test

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test projet</title>
+    <link rel="stylesheet" href="test.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php require("navbar.php"); ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <div class="container text-center center">
+        <div class="container">
+            <div class="row align-items-start">
+                <div class="col p-3 text-success-emphasis bg-success-subtle border border-success-subtle rounded-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar ac metus at suscipit. Nunc sit amet tortor eleifend, malesuada ex eu, facilisis justo. Nulla facilisi. Aenean rhoncus dapibus risus, vel fermentum felis. Duis massa lorem, interdum et dui vel, sodales condimentum nibh. Vivamus vestibulum id justo quis dictum. Donec et sem ipsum. Suspendisse sed elit finibus erat consequat pretium a quis nibh. Curabitur et odio nec magna iaculis interdum at in dolor. Nullam quis nisi quis diam finibus convallis. Integer accumsan pulvinar erat ac pretium. Praesent ultricies leo eget turpis placerat elementum. Nullam et consectetur risus, quis feugiat urna.
+
+                Pellentesque id lacus pretium, volutpat magna sit amet, venenatis quam. Duis leo erat, ornare quis viverra in, luctus a orci. Aenean in leo lobortis, vulputate sapien ac, convallis ligula. Proin tincidunt augue quis faucibus tempor. Sed facilisis lacus erat, egestas sagittis elit hendrerit vitae. Aliquam at tellus pulvinar, pharetra ligula quis, bibendum nisl. Vivamus sed sem est. Aliquam erat volutpat. Maecenas pharetra pulvinar elementum. Mauris convallis nibh vel urna elementum fringilla. Sed pretium orci nec ultricies fringilla. Aenean et gravida quam.
+
+                Morbi in volutpat massa, id elementum ex. Maecenas a tristique enim. Sed arcu erat, tristique sit amet enim ut, egestas sodales neque. Integer risus turpis, tincidunt at mollis in, fringilla rhoncus lectus. Nam eu leo tempor, finibus metus ac, facilisis neque. Vestibulum porta nisi nec arcu pretium imperdiet. Sed eros leo, ornare ac sapien quis, iaculis porta purus. Vestibulum eu eleifend lectus.
+
+                Quisque auctor nibh dui, et condimentum massa pulvinar eu. Proin lacus dolor, sodales sed porta nec, efficitur vitae quam. Integer ac sapien vehicula, ornare neque vitae, rutrum neque. In porta sed nisl sed interdum. Quisque posuere commodo metus, id egestas sapien tempus non. Nullam sollicitudin arcu enim, quis sodales massa faucibus sit amet. Sed a mattis erat. Quisque quis ipsum sed lorem cursus aliquet ut vitae enim. Mauris ac sollicitudin risus, sed mollis diam.
+
+                Maecenas pulvinar risus vitae congue lacinia. Integer justo urna, posuere et porttitor at, condimentum et turpis. Maecenas at egestas lectus. In vitae ante sodales, fringilla lorem et, euismod lorem. Aenean augue nulla, semper at urna in, malesuada gravida est. Nam velit nisi, auctor non eleifend vitae, dictum ac purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam non odio tortor. Quisque a ultricies lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla lectus risus, elementum nec justo sed, pulvinar ultrices dui.
+                </div>
+                <div class="col mx-auto d-block">
+                    <div class="card" style="width: 18rem;">
+                        <img src="test_card.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar ac metus at suscipit. Nunc sit amet tortor eleifend, malesuada ex eu, facilisis justo. Nulla facilisi. Aenean rhoncus dapibus risus, vel fermentum felis. Duis massa lorem, interdum et dui vel, sodales condimentum nibh. Vivamus vestibulum id justo quis dictum. Donec et sem ipsum. Suspendisse sed elit finibus erat consequat pretium a quis nibh. Curabitur et odio nec magna iaculis interdum at in dolor. Nullam quis nisi quis diam finibus convallis. Integer accumsan pulvinar erat ac pretium. Praesent ultricies leo eget turpis placerat elementum. Nullam et consectetur risus, quis feugiat urna.
+
+                Pellentesque id lacus pretium, volutpat magna sit amet, venenatis quam. Duis leo erat, ornare quis viverra in, luctus a orci. Aenean in leo lobortis, vulputate sapien ac, convallis ligula. Proin tincidunt augue quis faucibus tempor. Sed facilisis lacus erat, egestas sagittis elit hendrerit vitae. Aliquam at tellus pulvinar, pharetra ligula quis, bibendum nisl. Vivamus sed sem est. Aliquam erat volutpat. Maecenas pharetra pulvinar elementum. Mauris convallis nibh vel urna elementum fringilla. Sed pretium orci nec ultricies fringilla. Aenean et gravida quam.
+
+                Morbi in volutpat massa, id elementum ex. Maecenas a tristique enim. Sed arcu erat, tristique sit amet enim ut, egestas sodales neque. Integer risus turpis, tincidunt at mollis in, fringilla rhoncus lectus. Nam eu leo tempor, finibus metus ac, facilisis neque. Vestibulum porta nisi nec arcu pretium imperdiet. Sed eros leo, ornare ac sapien quis, iaculis porta purus. Vestibulum eu eleifend lectus.
+
+                Quisque auctor nibh dui, et condimentum massa pulvinar eu. Proin lacus dolor, sodales sed porta nec, efficitur vitae quam. Integer ac sapien vehicula, ornare neque vitae, rutrum neque. In porta sed nisl sed interdum. Quisque posuere commodo metus, id egestas sapien tempus non. Nullam sollicitudin arcu enim, quis sodales massa faucibus sit amet. Sed a mattis erat. Quisque quis ipsum sed lorem cursus aliquet ut vitae enim. Mauris ac sollicitudin risus, sed mollis diam.
+
+                Maecenas pulvinar risus vitae congue lacinia. Integer justo urna, posuere et porttitor at, condimentum et turpis. Maecenas at egestas lectus. In vitae ante sodales, fringilla lorem et, euismod lorem. Aenean augue nulla, semper at urna in, malesuada gravida est. Nam velit nisi, auctor non eleifend vitae, dictum ac purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam non odio tortor. Quisque a ultricies lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla lectus risus, elementum nec justo sed, pulvinar ultrices dui.
+                </div>
+            </div>
+        </div>
+        <div class="bd-exemple m-0 border-0">
+            <div class="bd-example">
+                <div class="form-floating">
+                    <form class="form-floating">
+                        <input type="email" class="form-control is-invalid" id="floatingInputInvalid" placeholder="name@example.com" value="test@example.com">
+                        <label for="floatingInputInvalid">Invalid input</label>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="form-floating">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea2">Comments</label>
+        </div>
+        <img src="test_card.jpeg" class="img-fluid" alt="...">
+
+
+        <div class="card">
+            <svg class="img" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="100%" height="100%" version="1.1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 784.37 1277.39" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g id="Layer_x0020_1">
+                    <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                    <g id="_1421394342400">
+                        <g>
+                            <polygon fill="#343434" fill-rule="nonzero" points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54"></polygon>
+                            <polygon fill="#8C8C8C" fill-rule="nonzero" points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33"></polygon>
+                            <polygon fill="#3C3C3B" fill-rule="nonzero" points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89"></polygon>
+                            <polygon fill="#8C8C8C" fill-rule="nonzero" points="392.07,1277.38 392.07,956.52 -0,724.89"></polygon>
+                            <polygon fill="#141414" fill-rule="nonzero" points="392.07,882.29 784.13,650.54 392.07,472.33"></polygon>
+                            <polygon fill="#393939" fill-rule="nonzero" points="0,650.54 392.07,882.29 392.07,472.33"></polygon>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+            <div class="textBox">
+                <p class="text head">Carte très style</p>
+                    <span>j'aime bien</span>
+                <p class="text price">pas compliqué</p>
+            </div>
+        </div>
+
+    </div>
+
+
+  </body>
+
+
+</html>
+  
