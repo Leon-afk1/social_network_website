b60e35f9f4ee7f973b08903040e1cf2e938d15c4
leon <leon.morales@utbm.fr>
2024-03-22 19:29:24 +0100

    Juste avec CSS sans bootstrap

--- BoutDePages/header.php
+++ BoutDePages/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-dark-subtle px-3 mb-3 mt-3 mx-3 sticky-top rounded-3">
+<!-- <nav class="navbar navbar-expand-lg bg-dark-subtle px-3 mb-3 mt-3 mx-3 sticky-top rounded-3">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"></a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -10,14 +10,14 @@
       <form class="d-flex" role="search">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <?php
-          if(isset($user)){
-            echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./logout.php'>Logout</a></li>";
-          } else {
-            echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./logout.php'>Logout</a></li>";
+          // if(isset($user)){
+          //   echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./logout.php'>Logout</a></li>";
+          // } else {
+          //   echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./logout.php'>Logout</a></li>";
 
-            echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./login.php'>Login</a></li>";
-            echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./sign_in.php'>Sign In</a></li>";
-          }
+          //   echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./login.php'>Login</a></li>";
+          //   echo "<li class='nav-item'><a class='nav-link active' aria-current='page' href='./sign_in.php'>Sign In</a></li>";
+          // }
         ?>
         
       </ul>
@@ -25,5 +25,28 @@
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
+  </div>
+</nav> -->
+
+<nav class="header">
+  <div class="navbar left">
+    <ul>
+      <li class="nav-item"><a class="nav-link active" aria-current="page" href="./index.php">Home</a></li>
+      <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">About</a></li>
+      <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Contact</a></li>
+    </ul>
+  </div>
+  
+  <div class="navbar right">
+    <ul>
+      <li class='nav-item'><a class='nav-link active' aria-current='page' href='./logout.php'>Logout</a></li>
+      <li class='nav-item'><a class='nav-link active' aria-current='page' href='./login.php'>Login</a></li>
+      <li class='nav-item'><a class='nav-link active' aria-current='page' href='./sign_in.php'>Sign In</a></li>
+    </ul>
+
+    <form role="search">
+      <input type="search" placeholder="Search" aria-label="Search">
+      <button type="submit">Search</button>
+    </form>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ConnectToDataBase();
 
 $AccountStatus = CheckLogin();
 if (!$AccountStatus["loginSuccessful"]){
-    echo "connected";
+    // echo "connected";
 }
 
 include ("BoutDePages/header.php");
@@ -14,31 +14,25 @@ include ("BoutDePages/header.php");
   <head>
     <title>My Page</title>
     <link rel="stylesheet" href="styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
   </head>
   <body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 
-    <main class="bg-secondary">
-        <div class="container text-center center">
-            <div class="row align-items-start">
-                <div class="col-md-6">
-                  <?php
-                    if (isset($_COOKIE['user_id'])) {
-                      echo "<h1>Welcome back, " . $_COOKIE['username'] . "!</h1>";
-                    } else {
-                      echo "<h1>Welcome to My Page</h1>";
-                    }
-                  ?>
-                    <h2>For you</h2>
-                    <ul id="trending-posts"></ul>
-                </div>
-                <div class="col-md-6">
-                    <h2>Following</h2>
-                    <ul id="following-posts"></ul>
-                </div>
-            </div>
-        </div>
+    <main>
+        <div>
+          <?php
+            if (isset($_COOKIE['user_id'])) {
+              echo "<h1>Welcome back, " . $_COOKIE['username'] . "!</h1>";
+            } else {
+              echo "<h1>Welcome to My Page</h1>";
+            }
+          ?>
+          <h2>For you</h2>
+          <ul id="trending-posts"></ul>
+      
+          <h2>Following</h2>
+          <ul id="following-posts"></ul>
     </main>
     <?php include ("BoutDePages/footer.php"); ?>
     <script src="script.js"></script>
